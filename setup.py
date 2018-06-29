83cddac8169dada545f26d164f17b7acec26c9f7
Zoeinfp <zoebelleton@gmail.com>
2018-06-29 12:45:13 +0200

    Adding build files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from cx_Freeze import *
+import os
+
+os.environ['TCL_LIBRARY'] = r'C:\Users\zwiim\Documents\macgyver\venv\Lib\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\zwiim\Documents\macgyver\venv\Lib\tcl8.6'
+
+base = None
+
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+target = Executable(
+    script="main.py",
+    base=base,
+    icon='item.ico',
+    )
+
+setup(
+    name="Mac Gyver Maze",
+    description="Run a maze with Mac Gyver and collect 3 items",
+    author="Zoe Belleton",
+    options={'build_exe': {'packages': ['pygame', 'pytmx', 'random']}},
+    executables=[target]
+)
+
+
